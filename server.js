6a095ab8bf5067903a131c2a9735cf40936c9304
Alex Launi <alexlauni@Alexs-MacBook-Pro.local>
2013-05-08 11:13:33 -0400

    Implement openLatchV1

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+var pif = require('caf_piface');
 
 var server = restify.createServer({
   name: 'Doorkit API',
@@ -17,11 +18,19 @@ server.use(restify.authorizationParser());
 
 var PORT = process.env.PORT || 5000;
 
+// Setup our PiFace to receive commands
+var piface = new pif.PiFace();
+piface.init();
 
 function openLatchV1(req, res, next) {
+    setTimeout(function () {
+        if (piface.read(0)) {
+            piface.write(0,0);
+        }, 10 * 1000); // reset the latch after 10 seconds
+    piface.write(1, 0);
     res.json({
-      message: 'not implemented yet',
-      code: 1234
+      message: 'Latch opened',
+      code: 0
     });
     return next();
 }
